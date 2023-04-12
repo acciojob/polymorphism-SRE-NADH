f5ceae918814f7dc7dc8dabaf6596d14d6ed13a6
SRE-NADH <sreenadh669@gmail.com>
2023-04-12 23:00:25 +0530

    first check

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
 
 public class Main {
+    static class product{
 
+        product(){}
+
+        public int product(int x,int y){
+            return x+y;
+        }
+        public int product(int x,int y,int z){
+            return x+y+z;
+        }
+        public double product(double x,double y){
+           return x+y;
+        }
+    }
+
+    public static void main(String[] args) {
+        product p = new product();
+        int ans1 = p.product(10,20);
+        int ans2 = p.product(10,20,30);
+        double ans3 = p.product(10.5,6.8);
+        System.out.println(ans1+" "+ans2+" "+ans3);
+    }
 }
