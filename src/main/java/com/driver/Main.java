1ff5d4b2331d3e05b00a6b3e782680d517c92e88
SRE-NADH <sreenadh669@gmail.com>
2023-04-14 12:42:32 +0530

    check3

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,10 +3,9 @@ package com.driver;
 public class Main {
     public static class product{
 
-       public product(){}
-
         public int product(int x,int y){
-            return x*y;
+
+           return x*y;
         }
         public int product(int x,int y,int z){
             return x*y*z;
