ad85ac11110b9dae831e0942c851fb6919fb9320
SRE-NADH <sreenadh669@gmail.com>
2023-04-14 12:46:35 +0530

    check4

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,7 +1,7 @@
 package com.driver;
 
 public class Main {
-    public static class product{
+    public static class Product{
 
         public int product(int x,int y){
 
@@ -16,7 +16,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        product p = new product();
+        Product p = new Product();
         int ans1 = p.product(10,20);
         int ans2 = p.product(10,20,30);
         double ans3 = p.product(10.5,6.8);
